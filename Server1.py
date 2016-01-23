2c59d6705e0afaaecfa2de7ede92821687d9c6d2
montague0324 <mcmontague@gmail.com>
2016-01-23 15:20:49 -0800

    Update Server1.py

--- Server1.py
+++ Server1.py
@@ -2,7 +2,7 @@ from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 
 PORT_NUMBER = 1234
 
-#This class will handles any incoming request from the browser 
+#Handle requests from browser
 class myHandler(BaseHTTPRequestHandler):
 	
   #Handler for the GET requests
@@ -11,23 +11,23 @@ class myHandler(BaseHTTPRequestHandler):
       self.send_response(200)
       self.send_header('Content-type','text/html')
       self.end_headers()
-      # Send the html message
-      self.wfile.write("Hello Mike!")
+      # Send html
+      self.wfile.write("Hello")
     except:
-      print "in the except clause!"
+      print "!!except clause!!"
     return
 
 def main():
   try:
-    #Create a web server and define the handler to manage the incoming request
+    #Create a web server and set handler
     server = HTTPServer(('', PORT_NUMBER), myHandler)
-    print 'Started httpserver on port ' , PORT_NUMBER, " use <Ctrl-C> to stop."
+    print 'Httpserver started on port ' , PORT_NUMBER, " use <Ctrl-C> to stop."
 	
-    #Wait forever for incoming htto requests
+    #Wait forever for incoming http requests
     server.serve_forever()
 
   except KeyboardInterrupt:
-    print '^C received, shutting down the web server'
+    print '^C received, shutting down server'
     server.socket.close()
 
 if __name__ == '__main__':
